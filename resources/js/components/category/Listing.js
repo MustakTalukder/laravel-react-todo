d234b90e4ca03616d5d1f078dabc120c90ec5d78
mustak <mustakhridoy@gmail.com>
2019-07-31 03:40:23 +0600

    category view

--- resources/js/components/category/Listing.js
+++ resources/js/components/category/Listing.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Listing extends Component {
+
+
+    constructor() {
+        super()
+
+        this.state = {
+            categories:[]
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8000/category')
+            .then(res => {
+                console.log(res.data);
+                
+                this.setState({
+                    categories: res.data
+                })
+            })
+    }
+    
+
+
     render() {
         return (
             <div>
@@ -15,28 +39,18 @@ export default class Listing extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Cat 1</td>
-                            <td>Active</td>
-                            <td>Date</td>
-                            <td>Date</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Cat 1</td>
-                            <td>Active</td>
-                            <td>Date</td>
-                            <td>Date</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Cat 1</td>
-                            <td>Active</td>
-                            <td>Date</td>
-                            <td>Date</td>
-                        </tr>
+                        {
+                            this.state.categories.map(category => 
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>{category.name}</td>
+                                    <td>{category.active}</td>
+                                    <td>{category.created_at}</td>
+                                    <td>{category.updated_at}</td>
+                                </tr>
+                            )
 
+                        }
                     </tbody>
                 </table>
             </div>
